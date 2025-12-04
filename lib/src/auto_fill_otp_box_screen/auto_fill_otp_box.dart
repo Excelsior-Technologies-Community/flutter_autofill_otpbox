0c82af2100712b83c318d60ca94c1b560e9bf1e8
VajaVishal <vishalvaja2622004@gmail.com>
2025-12-04 10:33:25 +0530

    frist_commit

---
+++ lib/src/auto_fill_otp_box_screen/auto_fill_otp_box.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AutoFillOtpBox extends StatefulWidget {
+  const AutoFillOtpBox({super.key});
+
+  @override
+  State<AutoFillOtpBox> createState() => _AutoFillOtpBoxState();
+}
+
+class _AutoFillOtpBoxState extends State<AutoFillOtpBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
